dca2595b57b5325b7c4cb88e9817afa078f18476
viniciusbalestra <vinicius.balestrin@hotmail.com>
2024-10-17 20:43:13 -0300

    Delete primeiro curso python directory

--- primeiro curso python/exercício.py
+++
@@ -1,6 +0,0 @@
-dados = {'nome': 'bob', 'idade': 23, 'cidade': 'cascavel'}
-dados.update({'idade': 28}) 
-#ou dados['idade'] = 28
-dados.update({'profissao': 'jornaleiro'})
-dados.pop('idade')
-print('nome' in dados)
